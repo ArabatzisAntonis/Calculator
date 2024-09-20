5c390161ad7ffc3a96937ac771b8e3b0257e9239
ArabatzisAntonis <arabatzisantonis@yahoo.com>
2024-09-20 15:11:30 +0300

    updated code

--- CALCULATOR/script.js
+++ CALCULATOR/script.js
@@ -1,7 +1,7 @@
     'use strict';
     
     function showonScreen(value) {
-            let screen = document.getElementById('ScreenTxtfld');
+            const screen = document.getElementById('ScreenTxtfld');
              
             if (screen.value === '0') {
                 screen.value = value;
@@ -13,41 +13,13 @@
         function clearScreen() {
             document.getElementById('ScreenTxtfld').value = '0';
         }
-        //exei errors
-        function addPercent() {
-            let screen = document.getElementById('ScreenTxtfld');
-            let value = screen.value;
         
-            
-            if (/\d$/.test(value)) {
-                
-                let lastNumberMatch = value.match(/(\d+(\.\d+)?)(?=[^\d]*$)/);
-        
-                if (lastNumberMatch) {
-                    let lastNumber = lastNumberMatch[0];
-        
-                    
-                    let precedingNumberMatch = value.match(/(\d+(\.\d+)?)(?=[^\d]*\d+(\.\d+)?[^\d]*$)/);
-        
-                    if (precedingNumberMatch) {
-                        let precedingNumber = precedingNumberMatch[0];
-        
-                        
-                        let newValue = value.replace(/(\d+(\.\d+)?)(?=[^\d]*$)/, `(${lastNumber} * 0.01 * ${precedingNumber})`);
-                        
-                        
-                        newValue = value.replace(/(\d+(\.\d+)?)(?=[^\d]*$)/, `((${lastNumber} * 0.01 * ${precedingNumber}))`);
-        
-                        screen.value = newValue;
-                    }
-                }
-            }
-        }
+    
     function Calculator() {
-    let screen = document.getElementById('ScreenTxtfld');
+        const screen = document.getElementById('ScreenTxtfld');
       try {
           screen.value = eval(screen.value) || '0';
-          screen.value = parseFloat(screen.value).toFixed(3);
+          //screen.value = parseFloat(screen.value).toFixed(3);
       }  catch (e) {
           screen.value = 'Error';
       }
